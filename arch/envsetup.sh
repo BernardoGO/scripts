841e1754f0928d2828e969a791c1ed964d227beb
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2018-10-16 15:42:37 -0300

    env setup

---
+++ arch/envsetup.sh
@@ -0,0 +1,36 @@
+sudo pacman -S --noconfirm yaourt
+
+sudo pacman -S --noconfirm code
+sudo pacman -S --noconfirm terminator
+yes | sudo pip install matplotlib
+yes | sudo pip install keras
+yes | sudo pip install numpy
+yes | sudo pip install selenium
+
+#Discord
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 0FC3042E345AD05D
+yaourt -S --noconfirm discord
+
+#Spotify
+yaourt -S --noconfirm spotify 
+
+
+yaourt -S --noconfirm automake
+yaourt -S --noconfirm make
+
+sudo pacman -S --noconfirm alsa-tools
+yaourt -S --noconfirm init-headphone
+#acpi_enforce_resources=lax
+yaourt -S --noconfirm grub-customizer
+
+
+yaourt -S --noconfirm vlc
+yaourt -S --noconfirm gimp
+
+yaourt -S --noconfirm inkscape
+yaourt -S --noconfirm davinci-resolve
+
+
+yaourt -S --noconfirm ttf-ms-fonts
+
+yaourt -S --noconfirm audacity

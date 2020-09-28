426cbfacfc6a304871ccacf07886298ac57461b8
Bernardo Augusto Godinho de Oliveira <bernardo.godinho.oliveira@gmail.com>
2020-09-28 14:26:27 -0300

    Create install_netflix.sh

---
+++ raspberry/install_netflix.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get dist-upgrade
+
+wget https://raw.githubusercontent.com/zjoasan/netflix-install-script/master/netflix_prep_install.sh
+chmod +x netflix_prep_install.sh
+./netflix_prep_install.sh

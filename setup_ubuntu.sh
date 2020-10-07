dd3e73008c956a4ab00c6f1a2c82113a42c7c00a
Bernardo Augusto Godinho de Oliveira <bernardo.godinho.oliveira@gmail.com>
2020-10-07 00:19:31 -0300

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -99,6 +99,10 @@ sudo apt-get --assume-yes install gimp
 sudo apt-get --assume-yes install inkscape
 #sudo apt-get --assume-yes install davinci-resolve
 
+echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
+sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
+sudo apt update
+sudo apt-get --assume-yes install balena-etcher-electron
 
 #####CONFIGS
 
